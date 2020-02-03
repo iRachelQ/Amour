a3ccecbe37f7b30ba4323e9f433cc4d2c0adb610
iRachelQ <1054352176@qq.com>
2020-02-03 18:37:45 +0800

    第二次提交，引入less，有一个react的源文件进行了修改，但是我未上库

---
+++ src/LoginMain/LoginMain.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import './LoginMain.less';
+
+/**
+ * 登录主页面
+ */
+export default class LoginMain extends Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    render(){
+        return (<div className="loginMain_main" >
+            <div className="loginMain_main_background"/>
+        </div>)
+    }
+
+}
